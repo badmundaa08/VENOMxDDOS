e348180a7bfb7d6691f8228cf1bce8959cec99ae
badmundaa08 <151648564+badmundaa08@users.noreply.github.com>
2024-07-28 15:18:49 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7476904793:AAEMQm39nDIepVlVP4t9cqqYHLaLbaeBQMo')
 
 # Admin user IDs
-admin_id = ["your id"]
+admin_id = ["5464427719"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
